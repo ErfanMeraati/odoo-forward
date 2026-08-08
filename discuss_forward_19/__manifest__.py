e03398c0718b88a4cc5729cb5ed2a1e38d14dc34
Erfan Meraati <161722964+ErfanMeraati@users.noreply.github.com>
2026-08-08 13:41:27 +0330

    Create __manifest__.py

---
+++ discuss_forward_19/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Discuss Message Forward',
+    'version': '19.0.1.0.0',
+    'category': 'Discuss',
+    'summary': 'Forward messages in Discuss',
+    'author': 'ErfanMeraati',
+    'website': 'https://meraati.net',
+    'license': 'LGPL-3',
+    'depends': ['mail'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'discuss_forward_19/static/src/js/**/*.js',
+            'discuss_forward_19/static/src/xml/**/*.xml',
+            'discuss_forward_19/static/src/scss/**/*.scss',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
